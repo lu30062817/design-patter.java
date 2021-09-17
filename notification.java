a01ac3760402ccdba9c3e153a985cf219ab6efac
lu30062817 <90741645+lu30062817@users.noreply.github.com>
2021-09-17 01:44:08 -0500

    Create notification.java

---
+++ notification.java
@@ -0,0 +1,12 @@
+public class Notification 
+{
+    final String NotificationContent;
+     
+    public Notification (String n) {
+        this.NotificationContent = n;
+    }
+ 
+    public String getNotificationContent() {
+        return NotificationContent;
+    }
+}
